f4c52909402c9a133a8ea8b3276eabf175eeb391
Kulani Baloyi <kulani17.kb@gmail.com>
2022-02-21 17:45:00 +0200

    Add DigitButton Component

---
+++ src/DigitButton.js
@@ -0,0 +1,14 @@
+import {ACTIONS} from "./App"; 
+
+function DigitButton({dispatch,digit}){
+    return(
+        <button className="number"
+            onClick={()=>dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit}})}
+        >
+            {digit}
+        </button>
+    )
+
+}
+
+export default DigitButton;  
